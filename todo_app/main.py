ad8a9375ef6028dd00048a44ff203d1c4f9c409d
PaulaMargon <pmartingc@gmail.com>
2025-06-20 18:37:37 +0100

    Se suben archivos bonus

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,10 +1,8 @@
 user_prompt = "Enter a todo:"
 
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+todos = []
 
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
-
-print(type(todos))
+while True:
+    todo = input(user_prompt)
+    todo.append()
+    todos.append(todo)
