d5733b5f106c9a676b40383b82c10318b1916da7
PaulaMargon <pmartingc@gmail.com>
2025-06-21 21:55:40 +0100

    Nuevos ejercicios

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,8 +1,26 @@
-user_prompt = "Enter a todo:"
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todo.append()
-    todos.append(todo)
+    user_action = input("Type add, show, edit or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo:")
+            todos.append(todo)
+
+        case 'show':
+            for item in todos:
+                print(item)
+
+        case 'edit':
+           number = int(input("Enter a number:"))
+           number = number -1
+           new_todo = input("Enter a new todo:")
+           todos[number] = new_todo
+
+        case 'exit':
+            break
+
+
+
